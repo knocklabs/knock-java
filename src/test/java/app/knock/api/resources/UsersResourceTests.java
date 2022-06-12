4e1b427e87d335f8a7fd5f3ebd3d4c877a3e1140
Jonathan <jonathan.kupcho@gmail.com>
2022-06-12 11:31:49 -0500

    fix: updated failing test, adding github action for build

--- src/test/java/app/knock/api/resources/UsersResourceTests.java
+++ src/test/java/app/knock/api/resources/UsersResourceTests.java
@@ -26,7 +26,7 @@ public class UsersResourceTests {
         String preferenceSetId = "pset12345";
 
         assertEquals("https://notreal.app/v1/users/user12345", this.usersResource.userUrl(userId).toString());
-        assertEquals("https://notreal.app/v1/users/user12345/channel_id/channel1234", this.usersResource.userChannelUrl(userId, channelId).toString());
+        assertEquals("https://notreal.app/v1/users/user12345/channel_data/channel1234", this.usersResource.userChannelUrl(userId, channelId).toString());
         assertEquals("https://notreal.app/v1/users/user12345/preferences", this.usersResource.userPreferencesUrl(userId).toString());
         assertEquals("https://notreal.app/v1/users/user12345/preferences/pset12345", this.usersResource.userPreferencesUrl(userId, preferenceSetId).toString());
     }
